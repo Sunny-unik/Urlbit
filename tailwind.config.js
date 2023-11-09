f12903f08009f220e74ceba0fd5778cc4ba3e0f1
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-11-10 00:12:39 +0530

    replace bootstrap to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,ejs}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
