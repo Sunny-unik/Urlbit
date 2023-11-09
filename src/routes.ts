ba899eebfa09f48678e879efe58c5ecdace5536c
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-11-09 20:06:48 +0530

    update contact & about pages

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ const urlRouter = Router();
 
 urlRouter.get("/", (req, res) => res.render("pages/home"));
 urlRouter.get("/about", (req, res) => res.render("pages/about"));
+urlRouter.get("/contact", (req, res) => res.render("pages/contact"));
 
 export default urlRouter;
