b9f908f7a96345f2e9ff48a28110ed6fa1cf9ba5
unknown <arsanyjoseph@gmail.com>
2023-11-20 21:45:11 +0200

    add port

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     service: process.env.SERVICE_PROVIDER,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER,
         pass: process.env.PASSWORD,
