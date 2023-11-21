c2daa86ca7c9557be8887f90db1982dd501041ea
unknown <arsanyjoseph@gmail.com>
2023-11-22 00:18:52 +0200

    add dotenv

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -1,7 +1,10 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config()
 
 const transporter = nodemailer.createTransport({
     service: process.env.SERVICE_PROVIDER,
+    host: process.env.HOST,
     port: 587,
     secure: false,
     auth: {
