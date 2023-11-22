158d350b3532f008f33dc0228790ed8cb9e82b74
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-11-23 00:19:45 +0530

    update contactForm height & timeOutMiddleware

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import ConnectDB from "./src/db";
 import urlRouter from "./src/routes";
 import path from "path";
-import { ExtendedError } from "src/interfaces/utils";
+import { timeOutMiddleware } from "./src/middlewares";
 
 dotenv.config();
 ConnectDB();
@@ -17,24 +17,7 @@ app.set("views", path.join(__dirname, "src/views"));
 app.use(express.static(__dirname + "/public"));
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  // Set 1min timeout for all HTTP requests
-  req.setTimeout(60000, () => {
-    const err = new Error("Request Timeout") as ExtendedError;
-    err.status = 408;
-    next(err);
-  });
-  // Set 1min server response timeout for all HTTP requests
-  res.setTimeout(60000, () => {
-    res.statusCode = 408;
-    res.send({
-      code: 408,
-      name: "REQUESTTIMEOUT",
-      message: "Request has timed out",
-    });
-  });
-  next();
-});
+app.use(timeOutMiddleware);
 app.use("/", urlRouter);
 
 app.get("/health", (req, res) => res.status(200).send("OK"));
