81efc1b0d9231036efa977c8aba595b87a25e8bc
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-11-24 23:38:24 +0530

    update prettier scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
         md: "768px",
         lg: "1024px",
         xl: "1280px",
-        "2xl": "1536px",
-      },
-    },
+        "2xl": "1536px"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
